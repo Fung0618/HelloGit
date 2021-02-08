7978a4b5cb0c06b3fc1fc9747b9630fc6f8780d3
Fung0618 <358052636@qq.com>
2021-02-08 12:00:30 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author shkstart
+ * @create 2021-02-08 11:41
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hellogit");
+    }
+}
